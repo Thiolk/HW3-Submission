7b9f77b0f62551613a3eea29c2342615d4376bb2
Thiolk <thiolengkiat@gmail.com>
2026-01-30 20:01:38 -0600

    fix seeding script

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO staging-task (description, completed) VALUES
+INSERT INTO task (description, completed) VALUES
   ('Staging Task A', 0),
   ('Staging Task B', 1),
   ('Staging Task C', 0);
